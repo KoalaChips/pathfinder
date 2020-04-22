21e7475e86f48e76d8cec129ae937d8d03cded6a
Raymond Lam <z5255290@unsw.edu.au>
2020-04-22 22:36:24 +1000

    Added different option

--- astar.py
+++ astar.py
@@ -6,19 +6,24 @@ tiles = [[0 for x in range(w)] for y in range(h)]
 state = 'w'
 
 def makeWall(i, j, event=None):
-    global tiles
-    tiles[i][j].frame.config(bg='gray5')
-
+    global tiles, state
+    states = {
+        'w': 'black', 
+        'p': 'white', 
+        's': 'green', 
+        'f': 'red'
+    }
+    tiles[i][j].state = state
+    tiles[i][j].frame.config(bg=states[state])
 def setstate(event):
     global state
     char = event.char
     states = ['w', 'p', 's', 'f']
     if char in states:
-        state = states
-    
+        state = char
 
 class tile(tk.Frame):
-    def __init__(self, h, w, i, j):
+    def __init__(self, h, w, i, j, state):
         self.frame = tk.Frame(root, 
                               height=h, 
                               width=w, 
@@ -29,7 +34,7 @@ class tile(tk.Frame):
                               bd=10)
         self.frame.grid(row=i, column=j)
         self.frame.bind("<Button-1>", lambda e: makeWall(i, j))
-        self.state = 'p'
+        self.state = state
 
 
 if __name__ == '__main__':
@@ -38,21 +43,21 @@ if __name__ == '__main__':
     height = 600
     size = str(width) + 'x' + str(height)
     root.geometry(size)
-    root.bind("<Key", setstate())
+    root.bind("<Key>", setstate)
 
     w, h = 30, 30
     tiles = [[0 for x in range(w)] for y in range(h)]
     for i in range(w):
         for j in range(h):
-            tiles[i][j] = tile(20, 20, i, j)
+            tiles[i][j] = tile(20, 20, i, j, 'p')
 
     root.mainloop()
 
-        '''
-        colour list:
-            calculated: green3
-            been to: red2
-            walkable: ghostwhite
-            wall: gray5
-            path: mediumturquoise
-        '''
+'''
+colour list:
+    calculated: green3
+    been to: red2
+    walkable: ghostwhite
+    wall: gray5
+    path: mediumturquoise
+'''
