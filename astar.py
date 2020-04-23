bdc1e4f5ecdc131eefb985ee8deba4b3ff51700e
Raymond Lam <z5255290@unsw.edu.au>
2020-04-23 21:35:17 +1000

    Made it possible to drag

--- astar.py
+++ astar.py
@@ -1,24 +1,30 @@
 import tkinter as tk
 import tkinter.messagebox as mbox
+
 import math
 
 w, h = 30, 30
 tiles = [[0 for x in range(w)] for y in range(h)]
 state = 'w'
-start = [0, 0]
-end = [0, 0]
+start = [-1, -1]
+end = [-1, -1]
+pos = [-1, -1]
 
-
-def makeWall(i, j, event=None):
+def makeWall(tile, event):
     global tiles, state, start, end
+    i = int((tile.i * tile.w + event.y) / tile.w)
+    j = int((tile.j * tile.w + event.x) / tile.w)
+
     if state == 's':
-        tiles[start[0]][start[1]].frame.config(bg='white')
-        tiles[start[0]][start[1]].state = 'p'
+        if start != [-1,-1]:
+            tiles[start[0]][start[1]].frame.config(bg='white')
+            tiles[start[0]][start[1]].state = 'p'
         start[0] = i
         start[1] = j
     elif state == 'f':
-        tiles[end[0]][end[1]].frame.config(bg='white')
-        tiles[end[0]][end[1]].state = 'p'
+        if end != [-1,-1]:
+            tiles[end[0]][end[1]].frame.config(bg='white')
+            tiles[end[0]][end[1]].state = 'p'
         end[0] = i
         end[1] = j
 
@@ -49,8 +55,12 @@ class tile(tk.Frame):
                               highlightthickness=1, 
                               bd=10)
         self.frame.grid(row=i, column=j)
-        self.frame.bind("<B1-Motion>", lambda e: makeWall(i, j))
         self.state = state
+        self.frame.bind('<Button-1>', lambda e: makeWall(self, e))
+        self.frame.bind("<B1-Motion>", lambda e: makeWall(self, e))
+        self.i = i
+        self.j = j
+        self.w = w
 
 
 if __name__ == '__main__':
@@ -60,12 +70,10 @@ if __name__ == '__main__':
     size = str(width) + 'x' + str(height)
     root.geometry(size)
     root.bind("<Key>", setstate)
-
-    w, h = 30, 30
-    tiles = [[0 for x in range(w)] for y in range(h)]
+    
     for i in range(w):
         for j in range(h):
-            tiles[i][j] = tile(20, 20, i, j, 'p')
+            tiles[i][j] = tile(w, h, i, j, 'p')
 
     root.mainloop()
 
