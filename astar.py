a94c4627d695b350e5d2c6ea6929112971d4a06b
Cameron <c.paterson410@gmail.com>
2020-04-23 14:31:03 +1000

    did drawing

--- astar.py
+++ astar.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import math
 
 root = tk.Tk(className="A* Pathfinding")
+cellX, cellY = 20, 20
 w, h = 30, 30
 tiles = [[0 for x in range(w)] for y in range(h)]
 state = 'w'
@@ -9,51 +10,45 @@ start = {'i':-1, 'j':-1}
 finish = {'i':-1, 'j':-1}
 status = False
 
-def changeStatus(event):
-    global status
-    if str(event.type) == "ButtonRelease":
-        status = False
-    elif str(event.type) == "ButtonPress":
-        status = True
-    print(status)
-
 
 def makeState(event, tile):
-    global tiles, start, finish, state, status, root
-    print(tile.row, tile.column)
-    if status:
-        i = tile.row
-        j = tile.column
+    global tiles, start, finish, state, root, width, height
+    x = tile.column * cellX + event.x
+    y = tile.row * cellY + event.y
+    i = math.floor(y/cellY)
+    j = math.floor(x/cellX)
+    print(x, y, w, h)
+    print(i, j)
 
-        if state == 'w':
-            colour = 'gray5'
-        elif state == 'p':
-            colour = 'ghostwhite'
-        elif state == 's':
+    if state == 'w':
+        colour = 'gray5'
+    elif state == 'p':
+        colour = 'ghostwhite'
+    elif state == 's':
 
-            if start['i'] != -1:
-                tiles[start['i']][start['j']].frame.config(bg='ghostwhite')
-                tiles[start['i']][start['j']].state = 'p'
-                start['i'] = i
-                start['j'] = j
-            else:
-                start['i'] = i
-                start['j'] = j
-            colour = 'purple'
+        if start['i'] != -1:
+            tiles[start['i']][start['j']].frame.config(bg='ghostwhite')
+            tiles[start['i']][start['j']].state = 'p'
+            start['i'] = i
+            start['j'] = j
+        else:
+            start['i'] = i
+            start['j'] = j
+        colour = 'purple'
 
-        elif state == 'f':
-            if finish['i'] != -1:
-                tiles[finish['i']][finish['j']].frame.config(bg='ghostwhite')
-                tiles[finish['i']][finish['j']].state = 'p'
-                finish['i'] = i
-                finish['j'] = j
-            else:
-                finish['i'] = i
-                finish['j'] = j
-            colour = 'orange'
+    elif state == 'f':
+        if finish['i'] != -1:
+            tiles[finish['i']][finish['j']].frame.config(bg='ghostwhite')
+            tiles[finish['i']][finish['j']].state = 'p'
+            finish['i'] = i
+            finish['j'] = j
+        else:
+            finish['i'] = i
+            finish['j'] = j
+        colour = 'orange'
 
-        tiles[i][j].frame.config(bg=colour)
-        tiles[i][j].state = state
+    tiles[i][j].frame.config(bg=colour)
+    tiles[i][j].state = state
 
 def setstate(event):
     global state
@@ -79,21 +74,20 @@ class tile(tk.Frame):
                               bd=10)
         self.frame.grid(row=i, column=j)
         self.state = 'p'
-        self.frame.bind("<Enter>", lambda event, obj=self: makeState(event, obj))
+        #self.frame.bind("<Enter>", lambda event, obj=self: makeState(event, obj))
+        self.frame.bind("<B1-Motion>", lambda event, obj=self: makeState(event, obj))
         self.row = i
         self.column = j
 
 
 if __name__ == '__main__':
-    width = 600
-    height = 600
-    size = str(width) + 'x' + str(height)
+    size = str(w * cellX) + 'x' + str(h * cellY)
     root.geometry(size)
     root.bind("<Key>", setstate)
 
-    
-    root.bind("<Button-1>", changeStatus)
-    root.bind("<ButtonRelease-1>", changeStatus)
+    #root.bind("<B1-Motion>", makeState)
+    #root.bind("<Button-1>", changeStatus)
+    #root.bind("<ButtonRelease-1>", changeStatus)
 
 
     w, h = 30, 30
